de0eb1f12c1e741f0e40de30227d804cd3ad06af
非空 <ren8179@126.com>
2017-10-19 17:34:21 +0800

    add vue admin

---
+++ src/Services/QrF.Core.UI/wwwroot/app/GodownEntry.cs
@@ -0,0 +1,72 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace QrFAbp.GodownEntryInfo
+{
+    /// <summary>
+    /// 入库单明细
+    /// </summary>
+    [Table("G_GodownEntry")]
+    public class GodownEntry : FullAuditedEntity, IMayHaveTenant
+    {
+        /// <summary>
+        /// 租户ID
+        /// </summary>
+        public int? TenantId { get; set; }
+
+        /// <summary>
+        /// 库房ID
+        /// </summary>
+        public int WareHouseId { get; set; }
+
+        /// <summary>
+        /// 发票号
+        /// </summary>
+        [MaxLength(20)]
+        public string InvoiceNo { get; set; }
+
+        /// <summary>
+        /// 经销商
+        /// </summary>
+        [MaxLength(100)]
+        public string Dealer { get; set; }
+
+        /// <summary>
+        /// 合同号
+        /// </summary>
+        [MaxLength(100)]
+        public string ContractNo { get; set; }
+
+        /// <summary>
+        /// 总金额
+        /// </summary>
+        public decimal TotalAmount { get; set; }
+
+        /// <summary>
+        /// 入库单明细
+        /// </summary>
+        public List<GodownEntryItem> Items { get; set; }
+
+        /// <summary>
+        /// 入库单状态
+        /// </summary>
+        public GodownEntryState GeState { get; set; }
+
+    }
+
+    /// <summary>
+    /// 入库单状态
+    /// </summary>
+    public enum GodownEntryState : byte
+    {
+        Saved = 0,
+        Submitted = 1,
+        Audited = 2
+    }
+
+}
---
+++ src/Services/QrF.Core.UI/wwwroot/src/router/index.js
@@ -0,0 +1,72 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const _import = require('./_import_' + process.env.NODE_ENV)
+// in development env not use Lazy Loading,because Lazy Loading too many pages will cause webpack hot update too slow.so only in production use Lazy Loading
+
+Vue.use(Router)
+
+/* layout */
+import Layout from '../views/layout/Layout'
+
+/**
+* icon : the icon show in the sidebar
+* hidden : if `hidden:true` will not show in the sidebar
+* redirect : if `redirect:noredirect` will no redirct in the levelbar
+* noDropdown : if `noDropdown:true` will has no submenu
+* meta : { role: ['admin'] }  will control the page role
+**/
+export const constantRouterMap = [
+    { path: '/login', component: _import('login/index'), hidden: true },
+    { path: '/authredirect', component: _import('login/authredirect'), hidden: true },
+    { path: '/404', component: _import('errorPage/404'), hidden: true },
+    { path: '/401', component: _import('errorPage/401'), hidden: true },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    name: '首页',
+    hidden: true,
+    children: [{ path: 'dashboard', component: _import('dashboard/index') }]
+  }
+]
+
+export default new Router({
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
+
+export const asyncRouterMap = [
+  {
+    path: '/Godown',
+    component: Layout,
+    icon: 'instore',
+    noDropdown: true,
+    children: [{ path: 'index', component: _import('instore/index'), name: '入库' }]
+  },
+  {
+    path: '/icon',
+    component: Layout,
+    icon: 'icon',
+    noDropdown: true,
+    children: [{ path: 'index', component: _import('svg-icons/index'), name: '图标' }]
+  },
+  {
+    path: '/theme',
+    component: Layout,
+    redirect: 'noredirect',
+    name: 'theme',
+    icon: 'theme',
+    noDropdown: true,
+    children: [{ path: 'index', component: _import('theme/index'), name: '换肤' }]
+  },
+  {
+    path: '/introduction',
+    component: Layout,
+    redirect: '/introduction/index',
+    icon: 'people',
+    noDropdown: true,
+    children: [{ path: 'index', component: _import('introduction/index'), name: '教程' }]
+  },
+  { path: '*', redirect: '/404', hidden: true }
+]
