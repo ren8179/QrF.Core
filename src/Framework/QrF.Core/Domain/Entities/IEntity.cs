f5aafd9aaee9dbcd89666ff47ae750351e443aad
RenQi <ren8179@126.com>
2018-04-13 13:28:46 +0800

    add entity

---
+++ src/Framework/QrF.Core/Domain/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QrF.Core.Domain.Entities
+{
+    public interface IEntity : IEntity<int>
+    {
+
+    }
+}
---
+++ src/Framework/QrF.Core/Domain/Entities/IEntityOfTPrimaryKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QrF.Core.Domain.Entities
+{
+    public interface IEntity<TPrimaryKey>
+    {
+        TPrimaryKey Id { get; set; }
+
+        bool IsTransient();
+    }
+}
