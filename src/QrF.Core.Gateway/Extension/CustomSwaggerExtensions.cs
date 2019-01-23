1a3d14661398c357a1015e46156c75bd564c2b05
RenQi <ren8179@126.com>
2019-01-23 23:30:01 +0800

    update startup

---
+++ src/QrF.Core.Gateway/Extension/CustomSwaggerExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QrF.Core.Gateway.Extension
+{
+    public static class CustomSwaggerExtensions
+    {
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(configuration["Name"], new Info
+                {
+                    Title = configuration["Title"],
+                    Version = configuration["Version"],
+                    Description = configuration["Description"]
+                });
+                options.OperationFilter<AddTokenHeaderParameter>();
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                if (Directory.Exists(basePath))
+                {
+                    var xmlPath = Path.Combine(basePath, $"{configuration["Name"]}.xml");
+                    if (File.Exists(xmlPath))
+                        options.IncludeXmlComments(xmlPath);
+                }
+            });
+
+            return services;
+        }
+    }
+}
--- src/QrF.Core.Gateway/Startup.cs
+++ src/QrF.Core.Gateway/Startup.cs
@@ -5,16 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using QrF.Core.Gateway.Extension;
 
 namespace QrF.Core.Gateway
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCustomSwagger(Configuration);
+            services.AddOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
