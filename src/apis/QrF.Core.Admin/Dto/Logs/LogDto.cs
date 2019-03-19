4961e4a6e3cec50169b1232b63bc5cacb9c6d38c
RenQi <ren8179@126.com>
2019-03-19 20:20:03 +0800

    add logdto

---
+++ src/apis/QrF.Core.Admin/Dto/Logs/LogDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QrF.Core.Admin.Dto
+{
+    public class LogDto
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 所属应用
+        /// </summary>
+        public string Application { get; set; }
+        /// <summary>
+        /// 业务类型
+        /// </summary>
+        public string BusinessType { get; set; }
+        /// <summary>
+        /// 业务编号
+        /// </summary>
+        public int? BusinessId { get; set; }
+        /// <summary>
+        /// 消息内容
+        /// </summary>           
+        public string Message { get; set; }
+        /// <summary>
+        /// 级别
+        /// </summary>
+        public string Level { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ src/apis/QrF.Core.Admin/Dto/Logs/QueryLogsInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QrF.Core.Admin.Dto
+{
+    public class QueryLogsInput : BasePageInput
+    {
+        /// <summary>
+        /// 级别
+        /// </summary>
+        public string Level { get; set; }
+    }
+}
